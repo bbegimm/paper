41034fb6cff1701c79b3302e52933dd4eed74392
bbegimm <80167084+bbegimm@users.noreply.github.com>
2022-04-08 08:35:43 +0300

    uploaded files

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// header sticky animation
+window.addEventListener("scroll", function () {
+    let header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0)
+})
+
+// Menu Burger
+const iconMenu = document.querySelector('.menu__icon');
+if (iconMenu) {
+    const menuBody = document.querySelector('.menu__body');
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    })
+}
